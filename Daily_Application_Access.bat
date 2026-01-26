94ec030d28411ec927b79ce93c812c460d1bcb85
Dharan-Nuthan-Kumar <115525532+dharannuthan@users.noreply.github.com>
2026-01-26 10:40:47 +0530

    Add files via upload

---
+++ Daily_Application_Access.bat
@@ -0,0 +1,264 @@
+echo on
+
+start chrome --incognito https://croatiadevcr.micnxt.com/croatiadevcr.html 
+
+echo Croatiadev cr accessed
+
+timeout 3
+
+start chrome --incognito https://croatiadev.micnxt.com/croatiadeven.html
+
+echo Croatiadev en accessed
+
+timeout 3
+
+start chrome --incognito https://croatiapreprod.micnxt.com/croatiapreprod.html 
+
+echo croatiapreprod cr accessed
+
+timeout 3
+
+start chrome --incognito https://croatiapreprod-en.micnxt.com/croatiapreprod-en.html
+
+
+echo croatiapreprod en accessed
+
+timeout 3
+
+start chrome --incognito https://oli.micnxt.com/oli.html 
+
+echo croatiaprod cr accessed
+
+timeout 3
+
+start chrome --incognito https://oli-en.micnxt.com/oli-en.html
+
+echo croatiaprod en accessed
+
+timeout 3
+
+start chrome --incognito https://migrainedev.micnxt.com/migrainedev.html
+
+echo Migraine dev accessed
+
+timeout 3
+
+start chrome --incognito https://sclerosisdev.micnxt.com/ai/sclerosisdev.html
+
+echo Sclerosis dev accessed
+
+timeout 3
+
+start chrome --incognito https://migrainetest.micnxt.com/
+
+echo Migraine test accessed
+
+timeout 3
+
+start chrome --incognito https://sclerosistest.micnxt.com/
+
+echo Sclerosis test accessed
+
+timeout 3
+
+start chrome --incognito https://mic-columbia-dev.rj.r.appspot.com/swagger-ui.html
+
+echo App api dev accessed
+
+timeout 3
+
+start chrome --incognito https://migrainepreprod.micnxt.com/migrainepreprod.html
+
+echo Migraine preprod accessed
+
+timeout 3
+
+start chrome --incognito https://sclerosispreprod.micnxt.com/sclerosispreprod.html
+
+echo scelorsis preprod accessed
+
+timeout 3
+
+start chrome --incognito https://mic-colombia-preprod.rj.r.appspot.com/swagger-ui.html
+
+echo App Api preprod accessed
+
+timeout 3
+
+start chrome --incognito https://www.cambiandovidasla.com/migrana
+
+echo migraine prod accessed
+
+timeout 3
+
+start chrome --incognito https://www.cambiandovidasla.com/esclerosis-multiple
+
+echo scelorsis prod accessed
+
+timeout 3
+
+start chrome --incognito https://mic-colombia-prod.rj.r.appspot.com/swagger-ui.html
+
+echo App api prod accessed
+
+timeout 3
+
+start chrome --incognito https://micapiarzdev.micnxt.com/
+
+echo Mic Arz accessed
+
+timeout 3
+
+start chrome --incognito mic-admin-ui-dot-mic-aaa-dev.uc.r.appspot.com
+
+echo Mic AAA accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharmadev.micnxt.com/valeopharmadev.html
+
+echo Valeopharma Dev chatbot accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharmadev-hcp.micnxt.com/login
+
+echo Valeopharma Dev Hcp accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharmatest-hcp.micnxt.com/login
+
+echo Valeopharma test hcp accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharmatest-chatbot.micnxt.com/valeopharmatest.html
+
+echo Valeopharma test chatbot accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharma-dev.nn.r.appspot.com/swagger-ui.html
+
+echo Valeopharma App api Dev accessed
+
+timeout 3
+
+start chrome --incognito https://valeopharma.nn.r.appspot.com/swagger-ui.html
+
+echo Valeopharma App api test accessed
+
+timeout 3
+
+start chrome --incognito https://doctafix-dev.micnxt.com
+
+echo Doctafix dev accessed
+
+timeout 3
+
+start chrome --incognito https://appapi-eyeflixdev-dot-eyeflix-canada.nn.r.appspot.com/swagger-ui.html
+
+echo Doctafix app api accessed
+
+timeout 3
+
+start chrome --incognito https://doctafix.micnxt.com
+
+echo Doctafix preprod accessed
+
+timeout 3
+
+start chrome --incognito https://eyeflix-canada.nn.r.appspot.com/swagger-ui.html
+
+echo Doctafix preprod app api accessed
+
+timeout 3
+
+start chrome --incognito https://34.100.207.86/testlink/index.php?caller=login&viewer=
+
+echo testlink accessed
+
+timeout 3
+
+start chrome --incognito https://apps.micnxt.com/jw/web/userview/appcenter/v/_/home
+
+echo Micnxt accessed
+
+timeout 3
+
+start chrome --incognito http://jenkins.micnxt.com
+
+echo jenkins accessed
+
+timeout 3
+
+start chrome --incognito https://34.100.207.86/mantisbt/login_page.php
+
+echo Mantisbt accessed
+
+timeout 3
+
+start chrome --incognito https://gilead.micnxt.com/gilead.html
+
+echo gilead chatbot accessed
+
+timeout 3
+
+start chrome --incognito https://gilead.micnxt.com
+
+echo gilead UI accessed
+
+timeout 3
+
+start chrome --incognito https://snapmoneydev.micnxt.com/
+
+echo snapmoneydev accessed
+
+timeout 3
+
+start chrome --incognito https://snapmoneytest.micnxt.com/
+
+echo snapmoneytest accessed
+
+timeout 3
+
+start chrome --incognito https://snapmoney.micnxt.com/
+
+echo snapmoneyprod accessed
+
+timeout 3
+
+start chrome --incognito https://statappdev.micnxt.com/#/
+
+echo statappdev accessed
+
+start chrome --incognito https://gtxzoedev-soyzoedev.cs201.force.com/ZOE/ZOE_LoginVfPage
+
+echo ZOE Dev accessed
+
+timeout 3
+
+start chrome --incognito https://novartis8--gtxzoetest.sandbox.my.site.com/ZOE/ZOE_LoginVfPage
+
+echo ZOE test accessed
+
+timeout 3
+
+start chrome --incognito https://novartis8--gtxzoeqa.sandbox.my.site.com/ZOE/ZOE_LoginVfPage
+
+echo ZOE QA accessed
+
+timeout 3
+
+start chrome --incognito https://soyzoe-portal.force.com/ZOE/ZOE_LoginVfPage
+
+echo ZOE PROD accessed
+
+timeout 3
+
+
+
+
+
+
